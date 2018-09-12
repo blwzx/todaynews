900982b8bcd365af41e564242799c9dd9c5b5d36
blwzx <1049345539@qq.com>
2018-09-12 17:52:11 +0800

    2

--- toutiao/controller/wechat/wechat.php
+++ toutiao/controller/wechat/wechat.php
@@ -1,12 +1,21 @@
 <?php
 include '../core/db.php';
-
-class wechat extends db
-{
-    public function feed()
-    {
+class wechat extends db{
+    public function feed(){
         $sql = 'select * from feed';
         $feeds = $this->pdo->query($sql)->fetchAll();
         echo json_encode($feeds);
     }
+    public function insert(){
+        $sql = 'insert into feed(user_name,user_avatar,content,publish_time,publish_address,img_urls) VALUES (?,?,?,?,?,?)';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1,$_GET['user_name']);
+        $stmt->bindValue(2,$_GET['user_avatar']);
+        $stmt->bindValue(3,$_GET['content']);
+        $stmt->bindValue(4,'2018.09.11');
+        $stmt->bindValue(5,'平阳路学府街');
+        $stmt->bindValue(6,'2');
+        echo $stmt ->execute();
+    }
+
 }
